bda1dfee029b2de81b797e48e5d9b0c8dbf5870e
Yu <3189109768@qq.com>
2019-12-26 17:46:56 +0800

    add sqlite3.c

---
+++ src/models/commdb/commdb.c
@@ -0,0 +1,8 @@
+#include "commdb.h"
+#include "sqlite3.h"
+
+ModuleCommDb commdbCtl = {
+    .version = 1,
+    .minor_version = 1,
+};
+
---
+++ src/models/include/commdb.h
@@ -0,0 +1,9 @@
+#ifndef _MY_COMMDB_H
+#define _MY_COMMDB_H 
+
+typedef struct Module_commdb
+{
+    int version;
+    int minor_version;
+} ModuleCommDb;
+#endif
